4e0cbf96e3220bcc2331b98a89f0f293702d0db0
Shakirov Ruslan <psevdo85@gmail.com>
2017-07-11 12:59:20 +0300

    Update prototype.php

--- creational/prototype.php
+++ creational/prototype.php
@@ -1,14 +1,36 @@
 <?php
 
+/*
+ * Встречаются ситуации, когда инициализация объекта некоторого класса занимает много ресурсов/времени. 
+ * В таком случае, для того чтобы избежать частого создания объектов этого путем инициализации, 
+ * используют клонирование уже существующих объектов-прототипов, такое решение называют шаблоном прототип.
+ */
 
-// method 1
-class A {
-	public function __construct(ClassA $prototype = null) {
-		if($prototype !== null) {
-		//
-		}
+interface Product {
+}
+
+class Factory {
+	private $product;
+
+	public function __construct(Product $product) {
+		$this->product = $product;
+	}
+
+	public function getProduct() {
+		return clone $this->product;
 	}
 }
 
-$prototype = new ClassA();
-$newObj = new ClassA($prototype);
+class SomeProduct implements Product {
+	public $name;
+}
+
+$prototypeFactory = new Factory(new SomeProduct());
+$firstProduct = $prototypeFactory->getProduct();
+$firstProduct->name = 'The first product';
+$secondProduct = $prototypeFactory->getProduct();
+$secondProduct->name = 'Second product';
+print_r($firstProduct->name);
+// The first product
+print_r($secondProduct->name);
+// Second product
